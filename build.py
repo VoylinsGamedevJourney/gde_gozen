92cc19d9f972219056c4af9b8b1c87c92e147590
voylin <0voylin0@gmail.com>
2024-07-20 08:53:36 +0900

    build update

--- build.py
+++ build.py
@@ -1,5 +1,4 @@
 import subprocess
-import readline
 
 # NOTE: You will need to compile the editor with 'dlink_enabled=yes' to have GDExtension support in web builds
 
@@ -18,37 +17,31 @@ if __name__ == '__main__':
 		case '4': platform = 'android'
 		case '5': platform = 'web'
 
-	# arch = ''
-	# if platform == 'linux':
-	# 	print('Select architexture:')
-	# 	print('1. x86_64;')
-	# 	print('2. arm64;')
-	# 	print('3. rv64.')
-	# 	match input('> '):
-	# 		case '1': arch = 'arch=x86_64'
-	# 		case '2': arch = 'arch=arm64'
-	# 		case '3': arch = 'arch=rv64'
-	# elif platform == 'windows':
-	# 	print('Select architexture:')
-	# 	print('1. x86_64;')
-	# 	print('2. x86_32.')
-	# 	match input('> '):
-	# 		case '1': arch = 'arch=x86_64'
-	# 		case '2': arch = 'arch=x86_32'
-	# elif platform == 'macos':
-	# 	print('Select architexture:')
-	# 	print('1. x86_64;')
-	# 	print('2. arm64.')
-	# 	match input('> '):
-	# 		case '1': arch = 'arch=x86_64'
-	# 		case '2': arch = 'arch=arm64'
-	# elif platform == 'android':
-	# 	print('Select architexture:')
-	# 	print('1. arm64;')
-	# 	print('2. arm32.')
-	# 	match input('> '):
-	# 		case '1': arch = 'arch=arm64'
-	# 		case '2': arch = 'arch=arm32'
+	arch = 'x86_64'
+	print('Select architexture:')
+	if platform == 'linux':
+		print('1. x86_64; (default)')
+		print('2. arm64;')
+		print('3. rv64.')
+		match input('> '):
+			case '2': arch = 'arm64'
+			case '3': arch = 'rv64'
+	elif platform == 'windows':
+		print('1. x86_64; (default)')
+		print('2. x86_32.')
+		match input('> '):
+			case '2': arch = 'x86_32'
+	elif platform == 'macos':
+		print('1. x86_64; (default)')
+		print('2. arm64.')
+		match input('> '):
+			case '2': arch = 'arm64'
+	elif platform == 'android':
+		print('1. arm64;')
+		print('2. arm32.')
+		match input('> '):
+			case '2': arch = 'arm32'
+			case _: arch = 'arm64'
 
 	target = 'debug'
 	print('Select target:')
@@ -57,10 +50,19 @@ if __name__ == '__main__':
 	match input('> '):
 		case '2': target = 'release'
 
+	extra_args = ''
+	if platform == 'linux':
+		print('Use system FFmpeg:')
+		print('1. No; (default)')
+		print('2. Yes;')
+		match input('> '):
+			case '2': extra_args = 'use_system=yes'
+			case _: extra_args = 'use_system=no'
+
 	user_input = input('Number of threads/cores for compiling> ')
 	if user_input.isdigit():
 		jobs = int(user_input)
 	else:
 		jobs = 1
 
-	subprocess.run(f'scons -j{jobs} target=template_{target} platform={platform}', shell=True, cwd='./')
+	subprocess.run(f'scons -j{jobs} target=template_{target} platform={platform} arch={arch} {extra_args}', shell=True, cwd='./')
