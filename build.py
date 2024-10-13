7ea3ecd962c485a99cd1b4db403d4a52fe3115a5
voylin <0voylin0@gmail.com>
2024-10-13 11:18:44 +0900

    Adding build option for test room

--- build.py
+++ build.py
@@ -3,49 +3,67 @@ import subprocess
 # NOTE: You will need to compile the editor with 'dlink_enabled=yes' to have GDExtension support in web builds
 
 if __name__ == '__main__':
+    platform = 'linux'
+    arch = 'x86_64'
+    target = 'debug'
+    extra_args = ''
+    use_system = False
+
     print('GoZen GDExtension builder')
     print('Select platform:')
     print('1. Linux; (default)')
     print('2. Windows;')
-    platform = 'linux'
-    match input('> '):
-        case '2': platform = 'windows'
 
-    arch = 'x86_64'
-    target = 'debug'
+    match input('> '):
+        case '2':
+            platform = 'windows'
+
     print('Select target:')
     print('1. Debug; (default)')
     print('2. Release.')
+
     match input('> '):
-        case '2': target = 'release'
+        case '2':
+            target = 'release'
+        case _:
+            extra_args += ' dev_build=yes'
 
-    extra_args = ''
-    if target == 'debug':
-        extra_args += ' dev_build=yes'
-
-    use_system = False
     if platform == 'linux':
         print('Use system FFmpeg:')
-        print('1. Yes; (default)')
-        print('2. No.')
+        print('1. No; (default)')
+        print('2. Yes.')
+
         match input('> '):
-            case '2': extra_args += ' use_system=no'
-            case _:
+            case '2':
                 extra_args += ' use_system=yes'
                 use_system = True
+            case _:
+                extra_args += ' use_system=no'
 
     if not use_system:
         print('Recompile FFmpeg:')
         print('1. Yes; (default)')
         print('2. No.')
+
         match input('> '):
-            case '2': extra_args += ' recompile_ffmpeg=no'
+            case '2':
+                extra_args += ' recompile_ffmpeg=no'
 
     user_input = input('Number of threads/cores for compiling> ')
     if user_input.isdigit():
         jobs = int(user_input)
     else:
         jobs = 1
+
+    print('Select location:')
+    print('1. Bin; (default)')
+    print('2. Test room.')
+
+    match input('> '):
+        case '2':
+            extra_args += ' location=test_room/addons/gde_gozen/bin'
+        case _:
+            extra_args += ' location=bin'
 
     subprocess.run(f'scons -j{jobs} target=template_{target} platform={platform} arch={arch} {extra_args}', shell=True, cwd='./')
 
