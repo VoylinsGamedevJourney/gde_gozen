87e79c240202585f0febffe24e30d1722587e291
voylin <0voylin0@gmail.com>
2024-11-20 17:57:51 +0900

    Init FFmpeg class

--- src/ffmpeg.hpp
+++ src/ffmpeg.hpp
@@ -1,5 +1,4 @@
-#pragma one
-
+#pragma once
 
 extern "C" {
 	#include <libavcodec/avcodec.h>
@@ -29,3 +28,22 @@ extern "C" {
 	#include <libswresample/swresample.h>
 	#include <libswscale/swscale.h>
 }
+
+#include <godot_cpp/classes/os.hpp>
+#include <godot_cpp/variant/utility_functions.hpp>
+
+
+using namespace godot;
+
+
+class FFmpeg {
+public:
+	static inline int response = 0;
+	static inline bool eof = false;
+
+
+	static void print_av_error(const char *a_message, int a_error);
+	static void enable_multithreading(AVCodecContext *&a_codec_ctx, const AVCodec *&a_codec);
+	static int get_frame(AVFormatContext *a_format_ctx, AVCodecContext *a_codec_ctx, int a_stream_id, AVFrame *a_frame, AVPacket *a_packet);
+	static enum AVPixelFormat get_hw_format(const enum AVPixelFormat *a_pix_fmt, enum AVPixelFormat *a_hw_pix_fmt);
+};
