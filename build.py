5414bcf7065ab5f09faebb80e9e4e2b70d156953
voylin <0voylin0@gmail.com>
2024-11-29 11:24:38 +0900

    Cleaned up build system

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 import subprocess
 
-# NOTE: You will need to compile the editor with 'dlink_enabled=yes' to have GDExtension support in web builds
+# NOTE: You will need to compile the editor with '' to have GDExtension support in web builds
 
 # Command for quick-building test:
 # scons -j10 target=template_debug dev_build=yes platform=linux arch=x86_64 location=test_room/addons/gde_gozen/bin use_system=no recompile_ffmpeg=no
@@ -13,58 +13,58 @@ if __name__ == '__main__':
     use_system = False
     gpl = ''
 
-    print('GoZen GDExtension builder')
-    print('Select platform:')
-    print('1. Linux; (default)')
-    print('2. Windows;')
-    print('3. MacOS;')
+    print('GoZen GDExtension builder\n'
+          'Select platform:\n'
+          '1. Linux; (default)\n'
+          '2. Windows;\n'
+          '3. MacOS; (Not working)\n'
+          '4. Web; (Not working)\n'
+          '4. Android; (Not working)')
 
     match input('> '):
-        case '2':
-            platform = 'windows'
-        case '3':
-            platform = 'macos'
+        case '2': platform = 'windows'
+        case '3': platform = 'macos'
+        case '3': platform = 'web dlink_enabled=yes'
+        case '3': platform = 'android'
 
-    print('Select target:')
-    print('1. Debug; (default)')
-    print('2. Release.')
+    print('Select target:\n'
+          '1. Debug; (default):\n'
+          '2. Release.')
 
     match input('> '):
-        case '2':
-            target = 'release'
-        case _:
-            extra_args += ' dev_build=yes'
+        case '2': target = 'release'
+        case _: extra_args += ' dev_build=yes'
 
     if platform == 'linux':
-        print('Use system FFmpeg:')
-        print('1. No; (default)')
-        print('2. Yes.')
+        print('Use system FFmpeg:\n'
+              '1. No; (default)\n'
+              '2. Yes.')
 
         match input('> '):
             case '2':
                 extra_args += ' use_system=yes'
                 use_system = True
-            case _:
-                extra_args += ' use_system=no'
+            case _: extra_args += ' use_system=no'
 
     if not use_system:
-        print('Recompile FFmpeg:')
-        print('1. Yes; (default)')
-        print('2. No.')
+        print('Recompile FFmpeg:\n',
+              '1. Yes; (default)\n',
+              '2. No.')
 
         match input('> '):
-            case '2':
-                extra_args += ' recompile_ffmpeg=no'
+            case '2': extra_args += ' recompile_ffmpeg=no'
+            case _:
+                extra_args += ' recompile_ffmpeg=yes'
 
-    print('Use GPL3:')
-    print('1. No; (default)')
-    print('2. Yes.')
+                print('Use GPL3:\n'
+                      '1. No; (default)\n'
+                      '2. Yes.')
 
-    match input('> '):
-        case '2':
-            extra_args += 'enable_gpl=yes'
-        case _:
-            extra_args += 'enable_gpl=no'
+                match input('> '):
+                    case '2':
+                        extra_args += 'enable_gpl=yes'
+                    case _:
+                        extra_args += 'enable_gpl=no'
 
     user_input = input('Number of threads/cores for compiling> ')
     if user_input.isdigit():
@@ -72,15 +72,14 @@ if __name__ == '__main__':
     else:
         jobs = 1
 
-    print('Select location:')
-    print('1. Bin; (default)')
-    print('2. Test room.')
+    print('Select location:\n'
+          '1. Bin; (default)\n'
+          '2. Test room.')
 
     match input('> '):
-        case '2':
-            extra_args += ' location=test_room/addons/gde_gozen/bin'
-        case _:
-            extra_args += ' location=bin'
+        case '2': extra_args += ' location=test_room/addons/gde_gozen/bin'
+        case _: extra_args += ' location=bin'
 
-    subprocess.run(f'scons -j{jobs} target=template_{target} platform={platform} arch={arch} {extra_args}', shell=True, cwd='./')
-
+    subprocess.run(f'scons -j{jobs} target=template_{target} '
+                   'platform={platform} arch={arch} {extra_args}',
+                   shell=True, cwd='./')
