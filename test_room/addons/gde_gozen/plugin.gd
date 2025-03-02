9bf8d4a4c188c108b488f2cc748f1e7c5e45558b
voylin <0voylin0@gmail.com>
2025-03-02 11:04:28 +0900

    Plugin update

--- test_room/addons/gde_gozen/plugin.gd
+++ test_room/addons/gde_gozen/plugin.gd
@@ -6,7 +6,10 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_custom_type("VideoPlayback", "Control", load("res://addons/gde_gozen/video_playback.gd"), load("res://addons/gde_gozen/icon.webp"))
+	add_custom_type(
+			"VideoPlayback", "Control",
+			load("res://addons/gde_gozen/video_playback.gd"),
+			load("res://addons/gde_gozen/icon.webp"))
 
 
 func _exit_tree() -> void:
